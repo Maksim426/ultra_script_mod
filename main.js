09500379f4963fc3da90a9b857b7f58e756fe9fd
Maksim426 <maksimpoliebko100@gmail.com>
2026-09-22 08:20:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
 (function() {
     var scope = (typeof vars !== 'undefined') ? this : (function() { return this; })();
-    var states = { 
-        god: false,
-        vacuum: false 
-    };
+    var states = { god: false, vacuum: false };
     
     // Память для восстановления честного прогресса исследований
     var savedResearch = []; 
 
     var printHelp = function() {
-        Log.info("\n=== ULTRASCRIPT V22.0 (FINAL EDITION) ===");
-        Log.info("creative : Ресурсы + Все исследования (toggle)");
-        Log.info("editor   : Редактор карты (toggle)");
-        Log.info("god      : Полное бессмертие + Щит (toggle)");
-        Log.info("instant  : Мгновенная стройка (toggle)");
-        Log.info("vacuum   : Авто-сбор ресурсов из буров в Ядро (toggle)");
-        Log.info("win      : Моментальная победа в секторе");
-        Log.info("kill     : Убить ВСЕХ врагов на карте");
-        Log.info("killnear : Убить врагов вокруг вас");
-        Log.info("fill     : Заполнить ядро предметами");
-        Log.info("ammo     : Зарядить все турели и дать энергию");
-        Log.info("dump     : Очистить ядро");
-        Log.info("heal     : Полноценно исцелить и потушить все постройки");
-        Log.info("tp       : Телепорт юнита к курсору");
-        Log.info("spawn('имя') : Заспавнить юнита (например: spawn('flare'))");
-        Log.info("off      : Выключить все читы и сбросить правила");
-        Log.info("=========================================");
+        Log.info("\n=== ULTRASCRIPT V21.0 ===");
+        Log.info("creative : Ресурсы + Все исследования");
+        Log.info("editor : Редактор");
+        Log.info("god : Бессмертие");
+        Log.info("vacuum : Авто-сбор ресурсов из буров в Ядро");
+        Log.info("instant : Мгновенная стройка");
+        Log.info("win : Захватить сектор");
+        Log.info("kill : Убить врагов");
+        Log.info("fill : Заполнить ядро");
+        Log.info("ammo : Заполнить все турели патронами");
+        Log.info("dump : Очистить ядро");
+        Log.info("heal : Исцелить постройки");
+        Log.info("spawn : Спавн юнита");
+        Log.info("off : Сброс настроек");
+        Log.info("=========================");
     };
 
-    // При высадке на карту выводим хелп
-    Events.on(Packages.mindustry.game.EventType.WorldLoadEvent, printHelp);
+    Events.on(EventType.WorldLoadEvent, printHelp);
 
-    // Постоянный цикл поддержки бессмертия и вакуума
-    Events.on(Packages.mindustry.game.EventType.Trigger.update, function() {
+    Events.on(EventType.UnitControlEvent, function() {
+        var u = Vars.player.unit();
+        if (u && states.god) {
+            u.health = u.maxHealth;
+            u.shield = 999999;
+        }
+    });
+
+    Events.on(EventType.Trigger.update, function() {
         if (!Vars.state.isGame()) return;
         
         var u = Vars.player.unit();
@@ -41,7 +42,6 @@
             u.shield = 999999;
         }
 
-        // Авто-перенос из буров
         if (states.vacuum && u && u.core()) {
             var core = u.core();
             Groups.build.each(function(b) {
@@ -57,21 +57,23 @@
 
     Object.defineProperty(scope, 'help', { get: function() { printHelp(); return ""; }, configurable: true });
 
-    // Идеальная команда creative
+    // Идеальная команда creative со взломом флага alwaysUnlocked
     Object.defineProperty(scope, 'creative', { get: function() { 
         Vars.state.rules.infiniteResources = !Vars.state.rules.infiniteResources; 
         
         if (Vars.state.rules.infiniteResources) {
-            savedResearch = [];
+            savedResearch = []; // Очищаем временную память перед записью
             
             Vars.content.each(function(c) {
                 if (c instanceof Packages.mindustry.type.UnlockableContent) {
+                    // Сохраняем исходное состояние, чтобы потом его вернуть
                     savedResearch.push({
                         content: c,
                         wasAlwaysUnlocked: c.alwaysUnlocked,
                         wasUnlocked: c.unlocked()
                     });
                     
+                    // Вламываемся в движок и делаем технологию "всегда доступной"
                     c.alwaysUnlocked = true;
                     c.unlock();
                     
@@ -80,18 +82,22 @@
                     }
                 }
             });
-            return "Creative: ON (Все технологии открыты!)";
+            return "Creative: ON (Все технологии разблокированы, замочки сняты!)";
         } else {
+            // Возвращаем все исследования к исходному честному состоянию
             for (var i = 0; i < savedResearch.length; i++) {
                 var data = savedResearch[i];
                 data.content.alwaysUnlocked = data.wasAlwaysUnlocked;
                 
-                if (!data.wasUnlocked && data.content.techNode) {
-                    data.content.techNode.unlocked = false;
+                if (!data.wasUnlocked) {
+                    // Если блок не был изучен ранее, закрываем его обратно
+                    if (data.content.techNode) {
+                        data.content.techNode.unlocked = false;
+                    }
                 }
             }
-            savedResearch = [];
-            return "Creative: OFF (Исследования возвращены)";
+            savedResearch = []; // Очищаем память
+            return "Creative: OFF (Технологии возвращены под замочки)";
         }
     }, configurable: true });
 
@@ -113,61 +119,34 @@
 
     Object.defineProperty(scope, 'vacuum', { get: function() { 
         states.vacuum = !states.vacuum; 
-        return "Vacuum Drill: " + (states.vacuum ? "ON" : "OFF"); 
+        return "Vacuum: " + (states.vacuum ? "ON" : "OFF"); 
     }, configurable: true });
     
-    Object.defineProperty(scope, 'instant', { get: function() { 
-        Vars.state.rules.buildSpeedMultiplier = (Vars.state.rules.buildSpeedMultiplier == 1 ? 9999 : 1); 
-        return "Instant Build: " + (Vars.state.rules.buildSpeedMultiplier > 1 ? "ON" : "OFF"); 
-    }, configurable: true });
-
-    Object.defineProperty(scope, 'tp', { get: function() { 
-        var u = Vars.player.unit();
-        if (u) {
-            u.set(Vars.player.mouseX, Vars.player.mouseY);
-            return "Телепортирован в point (" + Math.round(Vars.player.mouseX) + ", " + Math.round(Vars.player.mouseY) + ")";
-        }
-        return "Ошибка: нет юнита.";
-    }, configurable: true });
+    Object.defineProperty(scope, 'instant', { get: function() { Vars.state.rules.buildSpeedMultiplier = (Vars.state.rules.buildSpeedMultiplier == 1 ? 9999 : 1); return "Instant Build: " + (Vars.state.rules.buildSpeedMultiplier > 1 ? "ON" : "OFF"); }, configurable: true });
     
     Object.defineProperty(scope, 'win', { get: function() { 
         if (Vars.state.isCampaign()) {
             Vars.state.rules.victory = true;
             
             Groups.build.each(function(b) {
-                if (b.team != Vars.player.team()) b.kill();
+                if (b.team != Vars.player.team()) {
+                    b.kill();
+                }
             });
             
             Groups.unit.each(function(u) {
-                if (u.team != Vars.player.team()) u.kill();
+                if (u.team != Vars.player.team()) {
+                    u.kill();
+                }
             });
             
-            Events.fire(new Packages.mindustry.game.EventType.GameOverEvent(Vars.player.team()));
+            Events.fire(new EventType.GameOverEvent(Vars.player.team()));
             return "Сектор успешно захвачен! Запущен экран победы.";
         }
-        return "Ошибка: Работает только в режиме Кампании!";
+        return "Ошибка: Вы должны находиться в режиме Кампании!";
     }, configurable: true });
 
-    Object.defineProperty(scope, 'kill', { get: function() { 
-        var count = 0;
-        Groups.unit.each(function(u) { 
-            if (u.team != Vars.player.team()) { u.kill(); count++; } 
-        }); 
-        return "Уничтожено врагов: " + count; 
-    }, configurable: true });
-
-    Object.defineProperty(scope, 'killnear', { get: function() { 
-        var u = Vars.player.unit();
-        if (!u) return "Нет юнита";
-        var count = 0;
-        Groups.unit.each(function(other) { 
-            if (other.team != Vars.player.team() && other.within(u.x, u.y, 400)) { 
-                other.kill(); 
-                count++; 
-            } 
-        }); 
-        return "Уничтожено врагов в радиусе: " + count; 
-    }, configurable: true });
+    Object.defineProperty(scope, 'kill', { get: function() { Groups.unit.each(function(u) { if (u.team != Vars.player.team()) u.kill(); }); return "Враги уничтожены"; }, configurable: true });
     
     Object.defineProperty(scope, 'fill', { get: function() { 
         var unit = Vars.player.unit();
@@ -175,7 +154,7 @@
             Vars.content.items().each(function(i) { unit.core().items.set(i, unit.core().storageCapacity); });
             return "Ядро заполнено!"; 
         }
-        return "Ошибка: Ядро не найдено"; 
+        return "Нет ядра"; 
     }, configurable: true });
 
     Object.defineProperty(scope, 'ammo', { get: function() { 
@@ -183,44 +162,36 @@
         Groups.build.each(function(b) { 
             if (b.team == Vars.player.team() && b instanceof Packages.mindustry.world.blocks.defense.turrets.Turret.TurretBuild) {
                 var block = b.block;
+                
                 if (block.ammoTypes) {
                     Vars.content.items().each(function(i) {
                         if (block.ammoTypes.containsKey(i)) {
+                            for (var c = 0; c < block.maxAmmo; c++) {
+                                b.handleItem(null, i);
+                            }
                             b.items.set(i, block.itemCapacity);
                         }
                     });
                 }
+
                 if (b.liquids) {
                     Vars.content.liquids().each(function(l) {
                         b.liquids.set(l, block.liquidCapacity);
                     });
                 }
+
                 if (b.cons && b.cons.power) {
                     b.power.status = 1;
                 }
+                
                 count++;
             }
         });
-        return "Заряжено турелей: " + count; 
+        return "Все турели полностью заряжены! Всего: " + count; 
     }, configurable: true });
 
-    Object.defineProperty(scope, 'dump', { get: function() { 
-        var c = Vars.player.unit() ? Vars.player.unit().core() : null; 
-        if(c) { c.items.clear(); return "Ядро очищено"; } 
-        return "Ошибка: Ядро не найдено"; 
-    }, configurable: true });
-
-    Object.defineProperty(scope, 'heal', { get: function() { 
-        var count = 0;
-        Groups.build.each(function(b) { 
-            if (b.team == Vars.player.team()) {
-                b.health = b.maxHealth;
-                if (b.fire) b.fire.remove();
-                count++;
-            } 
-        }); 
-        return "Исцелено построек: " + count; 
-    }, configurable: true });
+    Object.defineProperty(scope, 'dump', { get: function() { var c = Vars.player.unit() ? Vars.player.unit().core() : null; if(c) { c.items.clear(); return "Ядро очищено"; } return "Нет ядра"; }, configurable: true });
+    Object.defineProperty(scope, 'heal', { get: function() { Groups.build.each(function(b) { if (b.team == Vars.player.team()) b.health = b.maxHealth; }); return "Постройки исцелены"; }, configurable: true });
     
     Object.defineProperty(scope, 'off', { get: function() { 
         states.god = false; 
@@ -230,6 +201,7 @@
         Vars.state.rules.buildSpeedMultiplier = 1; 
         if(Vars.player.unit()) Vars.player.unit().shield = 0; 
         
+        // Сброс исследований
         for (var i = 0; i < savedResearch.length; i++) {
             var data = savedResearch[i];
             data.content.alwaysUnlocked = data.wasAlwaysUnlocked;
@@ -238,20 +210,17 @@
             }
         }
         savedResearch = [];
-        return "Все чит-системы отключены"; 
+        return "Системы сброшены"; 
     }, configurable: true });
 
     scope.spawn = function(name) {
-        if (!name) {
-            return "Использование: spawn('название')\nПример: spawn('dagger') или spawn('flare')";
-        }
-        var type = Vars.content.units().find(function(u) { return u.name.toLowerCase().includes(name.toLowerCase()); });
+        var type = Vars.content.units().find(function(u) { return u.name.includes(name); });
         if (type) {
             var u = type.create(Vars.player.team());
             u.set(Vars.player.x, Vars.player.y);
             u.add();
-            return "Юнит [" + type.name + "] заспавнен рядом с вами!";
+            return type.name + " заспавнен!";
         }
-        return "Ошибка: Юнит с именем '" + name + "' не найден.";
+        return "Ошибка: юнит не найден.";
     };
 })();
